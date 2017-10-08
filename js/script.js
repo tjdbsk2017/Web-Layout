65f3948ac1f67adea9c667ce07d51e4be35cdbd9
tjdbsk2017 <31784320+tjdbsk2017@users.noreply.github.com>
2017-10-08 16:55:20 +0900

    portfolio

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    
+    $('.bxslider').bxSlider({
+    });
+  });
+    
+//
+//var doc = document;
+//var myVideo = doc.getElementById('myVideo')
+//var button = doc.getElementsByTagName('button');
+//var like = doc.querySelector('.like');
+//var likeP = like.getElementsByTagName('p');
+//var likeSpan = like.getElementsByTagName('span');
+//
+//function videoPlay (){
+//    
+//    if (myVideo.pause){
+//        myVideo.play();
+//    } else {
+//        myVideo.pause();
+//    }
+//    
+//    
+//}
+//
+//button.addEventListener('click', function(){
+//   this.classList.add('display');
+//    console.log('button');
+//});
+//
+//
+//like.addEventListener('click',function(){
+//    console.log('좋아요');
+//    likeP++;
+//});
+//
